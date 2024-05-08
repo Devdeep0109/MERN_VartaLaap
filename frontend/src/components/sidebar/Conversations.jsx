c59e533192d09367077cdd6bc8b23712884965dd
Devdeep0109 <devdeepgoyal@gmail.com>
2024-05-08 13:02:13 +0530

    structure file removed

--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -5,7 +5,7 @@ import Conversation from "./Conversation"
 const Conversations = () => {
 
   const {loading ,conversations}  = useGetConversations();
-  console.log( conversations);
+  // console.log( conversations);
 
   return (
     <div className="py-2 flex flex-col overflow-auto">
